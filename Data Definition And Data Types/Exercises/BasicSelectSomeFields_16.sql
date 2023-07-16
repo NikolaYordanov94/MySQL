059c62e17e54122a9c77bc32c5f5e982ea2a9aee
NikolaYordanov <nikola_bg94@abv.bg>
2023-07-17 00:32:34 +0300

    Initial Commit

---
+++ Data Definition And Data Types/Exercises/BasicSelectSomeFields_16.sql
@@ -0,0 +1,8 @@
+select `name` from towns
+order by `name` asc;
+
+select `name` from `departments`
+order by `name` asc;
+
+select `first_name`, `last_name`, `job_title`, `salary` from employees
+order by `salary` desc;
