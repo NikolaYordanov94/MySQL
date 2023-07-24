c08c6ec76b62e1360f6d79b1f546f5406ce322fe
NikolaYordanov <nikola_bg94@abv.bg>
2023-07-24 23:39:15 +0300

    Initial Commit

---
+++ Basic CRUD/Exercises/BiggestCountriesByPopulation_22.sql
@@ -0,0 +1,13 @@
+SELECT 
+    *
+FROM
+    `continents`;
+
+SELECT 
+    `country_name`, `population`
+FROM
+    `countries`
+WHERE
+    `continent_code` = 'EU'
+ORDER BY `population` DESC , `country_name` ASC
+LIMIT 30;
