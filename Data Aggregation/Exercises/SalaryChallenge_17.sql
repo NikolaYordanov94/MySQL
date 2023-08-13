b6ad3d57ee4d715476a0d4275362a257601dfea3
NikolaYordanov <nikola_bg94@abv.bg>
2023-08-14 01:19:39 +0300

    Initial Commit

---
+++ Data Aggregation/Exercises/SalaryChallenge_17.sql
@@ -0,0 +1,13 @@
+SELECT 
+    `first_name`, `last_name`, `department_id`
+FROM
+    `employees` e1
+WHERE
+    `salary` > (SELECT 
+            AVG(`salary`)
+        FROM
+            `employees` e2
+        WHERE
+            e1.department_id = e2.department_id)
+ORDER BY `department_id` , `employee_id`
+LIMIT 10;
