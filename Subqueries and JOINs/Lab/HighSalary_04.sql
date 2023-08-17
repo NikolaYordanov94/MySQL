4d6e35f133c7791034ce2343941f9a082651acb6
NikolaYordanov <nikola_bg94@abv.bg>
2023-08-18 00:26:25 +0300

    Initial Commit

---
+++ Subqueries and JOINs/Lab/HighSalary_04.sql
@@ -0,0 +1,9 @@
+SELECT 
+    COUNT(e.employee_id) AS count
+FROM
+    employees AS e
+WHERE
+    e.salary > (SELECT 
+            AVG(salary) AS average_salary
+        FROM
+            employees);
