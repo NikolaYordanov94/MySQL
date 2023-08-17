2a58e06e06eeb13eba2ead3d214df1715025cb7a
NikolaYordanov <nikola_bg94@abv.bg>
2023-08-18 00:25:37 +0300

    Initial Commit

---
+++ Subqueries and JOINs/Lab/TownsAndAddresses_02.sql
@@ -0,0 +1,9 @@
+SELECT 
+    a.town_id, t.`name` AS town_name, a.address_text
+FROM
+    addresses AS a
+        JOIN
+    towns AS t ON a.town_id = t.town_id
+WHERE
+    t.`name` IN ('San Francisco' , 'Sofia', 'Carnation')
+ORDER BY t.town_id ASC , a.address_id ASC;
