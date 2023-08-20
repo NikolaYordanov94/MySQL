5ee34ffb399b91cf4ecb9c2bf9d5af1838d72956
NikolaYordanov <nikola_bg94@abv.bg>
2023-08-20 11:43:03 +0300

    Initial Commit

---
+++ Subqueries and JOINs/Exercises/EmployeeManager_09.sql
@@ -0,0 +1,12 @@
+SELECT 
+    e.employee_id,
+    e.first_name,
+    e.manager_id,
+    m.first_name AS manager_name
+FROM
+    employees AS e
+        JOIN
+    employees AS m ON e.manager_id = m.employee_id
+WHERE
+    m.employee_id IN (3 , 7)
+ORDER BY e.first_name ASC;
