ef38e53d5dc1e63ee8620ece03bf53c4b2a91c77
NikolaYordanov <nikola_bg94@abv.bg>
2023-08-23 02:23:47 +0300

    Initial Commit

---
+++ Database Programmability and Transactions/Lab/CountEmployeesByTown_01.sql
@@ -0,0 +1,17 @@
+DELIMITER $$
+CREATE FUNCTION ufn_count_employees_by_town(town_name VARCHAR(255))
+RETURNS INT
+DETERMINISTIC
+  
+BEGIN
+  RETURN (
+  SELECT COUNT(e.employee_id)
+  FROM employees AS e
+  JOIN addresses AS a ON e.address_id = a.address_id
+  JOIN towns AS t ON a.town_id = t.town_id
+  WHERE t.`name` = town_name
+  );
+END $$
+DELIMITER ;
+
+SELECT ufn_count_employees_by_town('Sofia');
