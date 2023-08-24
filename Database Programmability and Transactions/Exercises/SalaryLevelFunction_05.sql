e734b53d96a6e428ba12c040edc47284fcf608ff
NikolaYordanov <nikola_bg94@abv.bg>
2023-08-24 10:18:09 +0300

    Initial Commit

---
+++ Database Programmability and Transactions/Exercises/SalaryLevelFunction_05.sql
@@ -0,0 +1,17 @@
+DELIMITER $$
+CREATE FUNCTION ufn_get_salary_level(salary DECIMAL(12, 2))
+RETURNS VARCHAR(7)
+DETERMINISTIC
+
+BEGIN
+  RETURN (
+  CASE 
+    WHEN salary < 30000 THEN 'Low'
+    WHEN salary <= 50000 THEN 'Average'
+    ELSE 'High'
+    END
+  );
+END $$
+DELIMITER ;
+
+SELECT UFN_GET_SALARY_LEVEL(50001);
