449d9c4303bc7233010dd9de2b45ce62893171e1
NikolaYordanov <nikola_bg94@abv.bg>
2023-08-25 16:55:28 +0300

    Initial Commit

---
+++ Database Programmability and Transactions/Exercises/FindFullName_08.sql
@@ -0,0 +1,12 @@
+DELIMITER $$
+CREATE PROCEDURE usp_get_holders_full_name()
+BEGIN
+SELECT 
+    CONCAT(first_name, ' ', last_name) AS full_name
+FROM
+    account_holders
+ORDER BY full_name ASC , id ASC;
+END $$
+DELIMITER ;
+
+call usp_get_holders_full_name();
