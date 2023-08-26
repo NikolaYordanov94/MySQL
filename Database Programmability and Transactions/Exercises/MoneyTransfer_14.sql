63f2e45fea164be02c61079438c7f785c21d9c35
NikolaYordanov <nikola_bg94@abv.bg>
2023-08-26 17:24:37 +0300

    Initial Commit

---
+++ Database Programmability and Transactions/Exercises/MoneyTransfer_14.sql
@@ -0,0 +1,52 @@
+DELIMITER $$
+CREATE PROCEDURE usp_transfer_money(
+    from_account_id INT, to_account_id INT, money_amount DECIMAL(19, 4))
+BEGIN
+    IF money_amount > 0 
+        AND from_account_id <> to_account_id 
+        AND (SELECT a.id 
+            FROM `accounts` AS a 
+            WHERE a.id = to_account_id) IS NOT NULL
+        AND (SELECT a.id 
+            FROM `accounts` AS a 
+            WHERE a.id = from_account_id) IS NOT NULL
+        AND (SELECT a.balance 
+            FROM `accounts` AS a 
+            WHERE a.id = from_account_id) >= money_amount
+    THEN
+        START TRANSACTION;
+        
+        UPDATE `accounts` AS a 
+        SET 
+            a.balance = a.balance + money_amount
+        WHERE
+            a.id = to_account_id;
+            
+        UPDATE `accounts` AS a 
+        SET 
+            a.balance = a.balance - money_amount
+        WHERE
+            a.id = from_account_id;
+        
+        IF (SELECT a.balance 
+            FROM `accounts` AS a 
+            WHERE a.id = from_account_id) < 0
+            THEN ROLLBACK;
+        ELSE
+            COMMIT;
+        END IF;
+    END IF;
+END $$
+DELIMITER ;
+
+CALL usp_transfer_money(1, 2, 10);
+CALL usp_transfer_money(2, 1, 10);
+
+SELECT 
+    a.id AS 'account_id', a.account_holder_id, a.balance
+FROM
+    `accounts` AS a
+WHERE
+    a.id IN (1 , 2);
+            
+DROP PROCEDURE IF EXISTS usp_transfer_money;
