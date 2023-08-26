7b3c93fa5c7af90ae59851749e383556f57a591f
NikolaYordanov <nikola_bg94@abv.bg>
2023-08-26 17:23:03 +0300

    Initial Commit

---
+++ Database Programmability and Transactions/Exercises/CalculatingInterest_11.sql
@@ -0,0 +1,18 @@
+DELIMITER $$
+CREATE PROCEDURE usp_calculate_future_value_for_account(
+    account_id INT, interest_rate DECIMAL(19, 4))
+BEGIN
+    SELECT 
+         a.id AS 'account_id', h.first_name, h.last_name, a.balance AS 'current_balance',
+         ufn_calculate_future_value(a.balance, interest_rate, 5) AS 'balance_in_5_years'
+    FROM
+        `account_holders` AS h
+            JOIN
+        `accounts` AS a ON h.id=a.account_holder_id
+    WHERE a.id = account_id;
+END $$
+DELIMITER ;
+
+CALL usp_calculate_future_value_for_account(1, 0.1);
+
+DROP PROCEDURE IF EXISTS usp_calculate_future_value_for_account;
