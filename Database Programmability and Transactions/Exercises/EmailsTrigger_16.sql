600088e07fa24af0b99a306d1216ddaa7bce86ed
NikolaYordanov <nikola_bg94@abv.bg>
2023-08-29 00:55:33 +0300

    Initial Commit

---
+++ Database Programmability and Transactions/Exercises/EmailsTrigger_16.sql
@@ -0,0 +1,28 @@
+CREATE TABLE `notification_emails` (
+    `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `recipient` INT(11) NOT NULL,
+    `subject` VARCHAR(50) NOT NULL,
+    `body` VARCHAR(255) NOT NULL
+);
+
+DELIMITER $$
+CREATE TRIGGER `tr_notification_emails`
+AFTER INSERT ON `logs`
+FOR EACH ROW
+BEGIN
+    INSERT INTO `notification_emails` 
+        (`recipient`, `subject`, `body`)
+    VALUES (
+        NEW.account_id, 
+        CONCAT('Balance change for account: ', NEW.account_id), 
+        CONCAT('On ', DATE_FORMAT(NOW(), '%b %d %Y at %r'), ' your balance was changed from ', ROUND(NEW.old_sum, 2), ' to ', ROUND(NEW.new_sum, 2), '.'));
+END $$
+DELIMITER ;
+
+SELECT 
+    *
+FROM
+    `notification_emails`;
+
+DROP TRIGGER IF EXISTS `bank`.tr_notification_emails;
+DROP TABLE IF EXISTS `notification_emails`;
