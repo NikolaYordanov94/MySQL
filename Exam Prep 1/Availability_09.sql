d4126a9df2f16f6224e4a34c1955b3d9b64fd0a3
NikolaYordanov <nikola_bg94@abv.bg>
2023-09-04 00:33:59 +0300

    Initial Commit

---
+++ Exam Prep 1/Availability_09.sql
@@ -0,0 +1,19 @@
+SELECT 
+    t.id AS table_id,
+    t.capacity,
+    COUNT(oc.client_id) AS count_clients,
+    CASE
+        WHEN t.capacity > COUNT(oc.client_id) THEN 'Free seats'
+        WHEN t.capacity = COUNT(oc.client_id) THEN 'Full'
+        ELSE 'Extra seats'
+    END AS availability
+FROM
+    `tables` AS t
+        JOIN
+    orders AS o ON t.id = o.table_id
+        JOIN
+    orders_clients AS oc ON o.id = oc.order_id
+WHERE
+    t.floor = 1
+GROUP BY t.id
+ORDER BY t.id DESC;
