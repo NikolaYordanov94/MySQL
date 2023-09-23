64e3ae15a1d037aa9a998751a87b39cb804568db
NikolaYordanov <nikola_bg94@abv.bg>
2023-09-23 17:48:23 +0300

    Initial Commit

---
+++ Database Basics MySQL Exam - 20 June 2021/Insert_02.sql
@@ -0,0 +1,10 @@
+use softuni_taxi_company_db;
+
+INSERT INTO clients (full_name, phone_number)
+SELECT 
+    CONCAT(first_name, ' ', last_name),
+    CONCAT('(088) 9999', (id * 2))
+FROM
+    drivers
+WHERE
+    id BETWEEN 10 AND 20;
